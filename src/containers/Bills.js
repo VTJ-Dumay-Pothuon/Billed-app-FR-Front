75ae2eb398dda44a880021f508e5ddb43ba91542
VTJ-Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2023-03-27 18:59:00 +0200

    23072023-1 : dates are strings, not ints

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -34,6 +34,7 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
+          .sort((a, b) => (a.date > b.date ? -1 : 1))
           .map(doc => {
             try {
               return {
@@ -52,7 +53,6 @@ export default class {
               }
             }
           })
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
         console.log('length', bills.length)
         return bills
       })
